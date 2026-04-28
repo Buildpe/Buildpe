462e9c4acf2fc8298b19bdb5bd996c80fb7ea8e7
Ali Raza <110591425+fantastic-raza@users.noreply.github.com>
2026-04-28 21:14:26 +0530

    Update PlatformToggle.jsx

--- src/components/common/PlatformToggle.jsx
+++ src/components/common/PlatformToggle.jsx
@@ -189,7 +189,7 @@ const PlatformToggle = ({ active = 'buildpe' }) => {
           </svg>
 
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', minWidth: 0 }}>
-            <span style={labelMain('buildpe')}>BuildPE</span>
+            <span style={labelMain('buildpe')}>Buildpe</span>
             <span style={labelSub('buildpe')}>Services &amp; Solutions</span>
           </div>
 
